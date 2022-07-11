7dda67fe7ef79f9f48574e6f35074044f2dd7135
Nhật Ngô Sách <nhat.ns@teko.vn>
2022-07-11 14:00:58 +0700

    init example server

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var port string
+
+func main() {
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	router := gin.Default()
+
+	router.GET("/health", health)
+
+	log.Fatal(router.Run(fmt.Sprintf("0.0.0.0:%s", port)))
+}
+
+func health(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
