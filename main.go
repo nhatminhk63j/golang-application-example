b6e89ee80c3bff5af22553a924d9c28f5793bfdd
Nhật Ngô Sách <nhat.ns@teko.vn>
2022-07-11 20:37:40 +0700

    add api argo

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/health", health)
+	router.GET("/argo", argo)
 
 	log.Fatal(router.Run(fmt.Sprintf("0.0.0.0:%s", port)))
 }
@@ -27,5 +28,12 @@ func main() {
 func health(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "ok",
+	})
+}
+
+func argo(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"success": "true",
+		"message": "our CI/CD pipeline is working",
 	})
 }
